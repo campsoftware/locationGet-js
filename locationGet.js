d8c633fe249cd9ef6489a45dc765861dedd8e27d
Hal Gumbert <hal@campsoftware.com>
2020-08-12 09:35:27 -0400

    Create locationGet.js

---
+++ locationGet.js
@@ -0,0 +1,37 @@
+function locationGet( callbackFunction ) {                                                                  
+    // Calling Example                                                                                      
+    // locationGet( function ( coords ) { alert( coords['ErrorCode'] + '|' + coords['Latitude'] ); } );     
+    if ( navigator.geolocation ) {                                                                          
+        navigator.geolocation.getCurrentPosition(                                                           
+            function ( position ) {                                                                         
+                let result = {};                                                                            
+                result[ 'ErrorCode' ] = "0";                                                                
+                result[ 'ErrorDesc' ] = "";                                                                 
+                result[ 'Latitude' ] = position.coords.latitude;                                            
+                result[ 'Longitude' ] = position.coords.longitude;                                          
+                result[ 'Altitude' ] = position.coords.altitude;                                            
+                result[ 'Heading' ] = position.coords.heading;                                              
+                result[ 'Speed' ] = position.coords.speed;                                                  
+                result[ 'Accuracy' ] = position.coords.accuracy;                                            
+                result[ 'AccuracyAltitude' ] = position.coords.altitudeAccuracy;                            
+                callbackFunction( result );                                                                 
+            },                                                                                              
+            function ( err ) {                                                                              
+                let result = {};                                                                            
+                result[ 'ErrorCode' ] = err.code;                                                           
+                result[ 'ErrorDesc' ] = err.message;                                                        
+                callbackFunction( result );                                                                 
+            },                                                                                              
+            {                                                                                               
+                enableHighAccuracy: true,                                                                   
+                timeout: 15000,                                                                             
+                maximumAge: 0                                                                               
+            }                                                                                               
+        )                                                                                                   
+    } else {                                                                                                
+        let result = {};                                                                                    
+        result[ 'ErrorCode' ] = -1;                                                                         
+        result[ 'ErrorDesc' ] = "Browser does not support Geolocation";                                     
+        callbackFunction( result );                                                                         
+    }                                                                                                       
+}                                                                                                           
